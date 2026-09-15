081d211439d7f4f85d00b3f8f106715f5c733256
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2026-09-15 15:44:35 +0700

    feat: wire health checker into server startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os/signal"
+	"syscall"
 
+	"github.com/hiroshiyoka/orpheus/internal/collector"
 	"github.com/hiroshiyoka/orpheus/internal/config"
 	"github.com/hiroshiyoka/orpheus/internal/storage"
 )
@@ -14,4 +18,14 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	projects, err := storage.ListProjects(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	collector.Start(ctx, db, projects, collector.Ping)
 }
